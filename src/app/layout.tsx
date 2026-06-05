77aaa381fce91270c49af7b4141e2cbacd2c2204
Mariano Eiberman <m.eiberman@happyfuncorp.com>
2026-06-05 12:36:11 -0300

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'BORED: The Video Game',
@@ -10,6 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className="bg-[#1a1a2e] text-white min-h-screen">{children}</body>
+      <Analytics />
     </html>
   );
 }
